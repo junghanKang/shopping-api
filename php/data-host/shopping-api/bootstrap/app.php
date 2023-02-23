5c0778c9f12ce530ac3fad0b48374eb7af56f238
Junghan Kang <junghan@Junghans-MacBook-Pro.local>
2023-02-23 17:00:16 +0900

    update bootstrap

--- php/data-host/shopping-api/bootstrap/app.php
+++ php/data-host/shopping-api/bootstrap/app.php
@@ -23,7 +23,7 @@ $app = new Laravel\Lumen\Application(
 
 $app->withFacades();
 
-// $app->withEloquent();
+$app->withEloquent();
 
 /*
 |--------------------------------------------------------------------------
@@ -91,7 +91,6 @@ $app->routeMiddleware([
 
 // $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
-$app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
 /*
