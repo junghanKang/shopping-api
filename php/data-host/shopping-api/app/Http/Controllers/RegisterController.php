436e1b0c7cc3026561b90feaf282f0184e70297a
Junghan Kang <junghan@Junghans-MacBook-Pro.local>
2023-02-23 16:58:28 +0900

    update register api

--- php/data-host/shopping-api/app/Http/Controllers/RegisterController.php
+++ php/data-host/shopping-api/app/Http/Controllers/RegisterController.php
@@ -4,33 +4,64 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Validator;
+
 
 class RegisterController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
+    // /**
+    //  * Create a new controller instance.
+    //  *
+    //  * @return void
+    //  */
+    // public function __construct()
+    // {
+    //     //
+    // }
 
+    /**
+     * Register a new user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function registerUser(Request $request)
     {
-        $name = $request->input('name');
-        $nickname = $request->input('nickname');
-        $password = $request->input('password');
-        $phone = $request->input('phone');
-        $email = $request->input('email');
-        $gender = $request->input('gender', '');
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:20', 'regex:/^[a-zA-Z가-힣\s]+$/u'],
+            'nickname' => ['required', 'string', 'max:30', 'regex:/^[a-z\s]+$/'],
+            'password' => [
+                'required',
+                'string',
+                'min:10',
+                'regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]+$/'
+            ],
+            'phone' => ['required', 'string', 'max:20'],
+            'email' => ['required', 'string', 'email', 'max:100', 'unique:users,email'],
+            'gender' => ['nullable', Rule::in(['male', 'female', 'other'])],
+        ]);
 
-        DB::insert(
-                'insert into users (name, nickname, password, phone, email, gender) values (?, ?, ?, ?, ?, ?)',
-                [$name, $nickname, $password, $phone, $email, $gender]
-            );
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
 
-        return response()->json(['message' => 'Request successful']);
+        $password = Hash::make($request->input('password'));
+
+        $user = DB::table('users')->insertGetId([
+            'name' => $request->input('name'),
+            'nickname' => $request->input('nickname'),
+            'password' => $password,
+            'phone' => $request->input('phone'),
+            'email' => $request->input('email'),
+            'gender' => $request->input('gender', ''),
+        ]);
+
+        if ($user) {
+            return response()->json(['message' => 'User registered successfully'], 201);
+        }
+
+        return response()->json(['message' => 'Failed to register user'], 500);
     }
 }
